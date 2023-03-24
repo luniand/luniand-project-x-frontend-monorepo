a0b968d82036555f7f1e59800d9c2524cb45bd10
Toan Tran <trantoan.fox.97@gmail.com>
2023-03-25 00:30:42 +0700

    fix(nuxt-config): add head information for app

--- apps/students/nuxt.config.ts
+++ apps/students/nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width,initial-scale=1",
+      title: "Luniand | Road To Your Career Path",
+      titleTemplate: "%s - Luniand",
+      meta: [
+        {
+          name: "description",
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+      ],
+    },
+  },
+
+  extends: ["nuxt-seo-kit"],
+
   // modules: ["luniand-nuxt3-kit"],
 
   runtimeConfig: {
