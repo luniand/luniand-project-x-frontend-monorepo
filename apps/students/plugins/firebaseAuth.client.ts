3374b7ed554e100c935371b4e28bb3728636cf3c
hong-IT-99 <56642090+hong-IT-99@users.noreply.github.com>
2023-03-24 22:18:05 +0700

    feat(auth): update save state auth and add middleware auth

--- apps/students/plugins/firebaseAuth.client.ts
+++ apps/students/plugins/firebaseAuth.client.ts
@@ -1,11 +1,17 @@
+
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
-
   const firebaseConfig = {
     apiKey: config.FIREBASE_API_KEY,
   };
 
   const app = initializeApp(firebaseConfig);
+  initUser()
+
+  const auth = getAuth()
+  nuxtApp.vueApp, provide('auth', auth)
+  nuxtApp.provide('auth', auth)
 });
