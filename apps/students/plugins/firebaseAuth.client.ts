1bf66c5f18aedfd01315eef3553660440031d8ad
hong-IT-99 <56642090+hong-IT-99@users.noreply.github.com>
2023-03-25 00:41:56 +0700

    feat(auth): add login with google and github

--- apps/students/plugins/firebaseAuth.client.ts
+++ apps/students/plugins/firebaseAuth.client.ts
@@ -1,17 +1,22 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
+
   const firebaseConfig = {
     apiKey: config.FIREBASE_API_KEY,
+    authDomain: "nuxt3-firebase-f6787.firebaseapp.com",
+    projectId: "nuxt3-firebase-f6787",
+    storageBucket: "nuxt3-firebase-f6787.appspot.com",
+    messagingSenderId: "667086197122",
+    appId: "1:667086197122:web:cc94ecc91bd991810b7f9b",
   };
 
   const app = initializeApp(firebaseConfig);
-  initUser()
+  initUser();
 
-  const auth = getAuth()
-  nuxtApp.vueApp, provide('auth', auth)
-  nuxtApp.provide('auth', auth)
+  const auth = getAuth();
+  nuxtApp.vueApp, provide("auth", auth);
+  nuxtApp.provide("auth", auth);
 });
