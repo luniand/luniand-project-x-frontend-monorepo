9681d209e559b1bb7e17adb4b841ce7972bd99da
hong-IT-99 <56642090+hong-IT-99@users.noreply.github.com>
2023-03-24 22:18:05 +0700

    feat(auth): update save state auth and add middleware auth

--- apps/students/composables/useFirebase.ts
+++ apps/students/composables/useFirebase.ts
@@ -6,8 +6,10 @@ import {
   signInWithEmailAndPassword,
   onAuthStateChanged,
 } from "firebase/auth";
+import { FormData } from "~~/types/typeAuth";
+import { useStateFirebase } from "./useStateFireBase";
 
-export const createUser = async (formData: any) => {
+export const createUser = async (formData: FormData) => {
   const { email, password } = formData;
   const auth = getAuth();
   const credentials = await createUserWithEmailAndPassword(
@@ -21,7 +23,7 @@ export const createUser = async (formData: any) => {
   return credentials;
 };
 
-export const signInUser = async (formData: any) => {
+export const signInUser = async (formData: FormData) => {
   const { email, password } = formData;
   const auth = getAuth();
   const credentials = await signInWithEmailAndPassword(
@@ -37,10 +39,27 @@ export const signInUser = async (formData: any) => {
 
 export const initUser = async () => {
   const auth = getAuth();
+  const firebaseState = useStateFirebase()
+  firebaseState.value = auth.currentUser
+
+  const userCookie = useCookie('useCookie')
   onAuthStateChanged(auth, (user) => {
     if (user) {
       const uid = user.uid;
+
     } else {
+
     }
+    firebaseState.value = user
+
+    userCookie.value = user
   });
 };
+
+
+
+export const signOut = async () => {
+  const auth = getAuth()
+  const result = await auth.signOut()
+  console.log(result);
+}
