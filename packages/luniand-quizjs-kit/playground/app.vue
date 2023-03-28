365fc99a401a2c5eb6ee598bb513a697b20180f2
Toan Tran <trantoan.fox.97@gmail.com>
2023-03-28 22:53:08 +0700

    fix(quizjs): add app structure from nuxt3 kit

---
+++ packages/luniand-quizjs-kit/playground/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <h1><base-quiz /></h1>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ packages/luniand-quizjs-kit/src/runtime/base-quiz.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Base Quiz</h1>
+    <h2>Sub Quiz</h2>
+  </div>
+</template>
