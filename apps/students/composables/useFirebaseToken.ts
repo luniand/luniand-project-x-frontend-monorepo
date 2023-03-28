9ff91968dc0deebd3257cb2c745a63cfd52c2466
Toan Tran <trantoan.fox.97@gmail.com>
2023-03-28 20:52:56 +0700

    fix(auth): token lost session in development mode

--- apps/students/composables/useFirebaseToken.ts
+++
@@ -1,6 +0,0 @@
-import { useCookie, useRuntimeConfig } from "#imports";
-
-export default function () {
-  const cookieOptions = useRuntimeConfig().public.FIREBASE_AUTH_COOKIE;
-  return useCookie(`${cookieOptions.name}-token`);
-}
